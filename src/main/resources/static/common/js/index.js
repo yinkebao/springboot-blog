361f62ce2d7a6211b84f242522d411d4645fbd5d
yinkebao <yinkebao@hztianque.com>
2019-11-18 20:28:41 +0800

    调整主页

--- src/main/resources/static/common/js/index.js
+++ src/main/resources/static/common/js/index.js
@@ -1,7 +1,7 @@
 
 $(function () {
     initLayui();
-    initGirl();
+    init2DImg();
 });
 
 //初始化layui
@@ -13,11 +13,16 @@ function initLayui(){
     }();
 }
 
-function initGirl() {
-    L2Dwidget.init({ "model": { jsonPath:
-                "https://unpkg.com/live2d-widget-model-shizuku@1.0.5/assets/shizuku.model.json",
-            "scale": 1 }, "display": { "position": "left", "width": 150, "height": 300,
-            "hOffset": 0, "vOffset": -20 }, "mobile": { "show": true, "scale": 0.5 },
-        "react": { "opacityDefault": 0.7, "opacityOnHover": 0.2 }
+function init2DImg() {
+    L2Dwidget.init({
+        "model": {
+            jsonPath:
+                "https://unpkg.com/live2d-widget-model-koharu@1.0.5/assets/koharu.model.json",
+            "scale": 1
+        }, "display": {
+            "position": "left", "width": 85, "height": 220,
+            "hOffset": 0, "vOffset": -20
+        }, "mobile": {"show": true, "scale": 0.5},
+        "react": {"opacityDefault": 0.7, "opacityOnHover": 0.2}
     });
 }
